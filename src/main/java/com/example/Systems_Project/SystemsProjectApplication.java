b282edb0fb18d678c746f032a68437b3794a6f3a
karen-velasquez <54156284+karen-velasquez@users.noreply.github.com>
2020-03-09 00:23:58 -0400

    Initial commit

---
+++ src/main/java/com/example/Systems_Project/SystemsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Systems_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemsProjectApplication.class, args);
+	}
+
+}
